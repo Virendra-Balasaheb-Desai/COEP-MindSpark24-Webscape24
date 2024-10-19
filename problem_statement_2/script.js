a9359327068edfc0dda3f0c911fa17e558d3a32d
Virendr-Balasaheb-Desai <desaivirendra959@gmail.com>
2024-10-19 13:49:27 +0530

    Adding the animation

--- problem_statement_2/script.js
+++ problem_statement_2/script.js
@@ -1,6 +1,20 @@
-gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('.box', {
-    scrollTrigger: '.box', 
-    x: 500
+const observer = new IntersectionObserver((entry) => {
+    
+   if(entry[0].isIntersecting)
+   {
+      console.log(entry.target);
+      
+      entry[0].target.classList.add("show");
+   }
 });
+
+const sections = document.querySelectorAll('.sec2');
+// Array.from(sections).forEach(()={
+    
+// })
+sections.forEach((e)=>{
+
+    observer.observe(e);
+})
+
